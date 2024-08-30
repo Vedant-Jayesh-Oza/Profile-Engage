205afa9b25639a75e8a0bb1bfa4fe99857ee579c
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2024-08-29 23:39:50 -0700

    Profile-Engage-projectFile

---
+++ third_parties/linkedin.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/Vedant-Jayesh-Oza/473223423fa140ba803a75f0557aaa07/raw/7984385380324a150a0af17a279089550638025c/vedantoza.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+        response = requests.get(
+            api_endpoint,
+            params={"url": linkedin_profile_url},
+            headers=header_dic,
+            timeout=10,
+        )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/vedant-oza-8354a4200/",
+
+        )
+    )
